544ed9de2cf732cb1f6de32b9486fe96a71f3ff1
Mary San Agustin <marycsanagustin@gmail.com>
2019-02-20 11:54:46 -0800

    Latest form validation draft

--- dist/js/main.js
+++ dist/js/main.js
@@ -18,6 +18,17 @@ const changeImage = event => {
 // ----------------------------------------------------------
 // ORDER OPTIONS & CART
 
+let orderOBJ = {
+  cart: {
+    products: "",
+    total: {
+      sub_total: "",
+      shipping: "$10.25",
+      grand_total: ""
+    }
+  }
+};
+
 // Adds green background and border when selected
 const toggleOption = option => {
   // Set initial state of option
@@ -32,31 +43,7 @@ const toggleOption = option => {
   extractPrice(option.parentNode);
 };
 
-// TODO: Fix null error when clicking anything within option div that is an image
-
 // TODO: Re-calculate total when product is "deslected"
-
-let orderOBJ = {
-  cart: {
-    products: "",
-    total: {
-      sub_total: "",
-      shipping: "$10.25",
-      grand_total: ""
-    }
-  }
-};
-
-const logCart = (products, cartSum, withShipping) => {
-  orderOBJ.cart["products"] = products;
-  orderOBJ.cart.total["sub_total"] = `$${cartSum}`;
-  orderOBJ.cart.total["grand_total"] = `$${withShipping}`;
-
-  const cart = Object.assign({}, orderOBJ);
-  const orderJSON = JSON.stringify(cart);
-
-  console.log(orderJSON);
-};
 
 const extractPrice = option => {
   // Find the h4 element that contains the price
@@ -76,34 +63,15 @@ const extractPrice = option => {
   // If option is selected, add its price to the sessionStorage
   if (option.classList.contains("selected")) {
     sessionStorage.setItem(name, priceNum);
-    const cartNames = Object.keys(sessionStorage);
-    const cartPrices = Object.values(sessionStorage);
-    const cartNums = cartPrices.map(el => parseFloat(el));
-    const cartSum = cartNums.reduce((sum, amount) => sum + amount);
-    const products = Object.assign({});
-    const shipping = 10.25;
-
-    // For every product, associate it with its corresponding price
-    cartNames.forEach(name => {
-      cartPrices.forEach(price => {
-        products[name] = price;
-      });
-    });
-
-    // Add Shipping
-    let withShipping = cartSum + shipping;
-
-    // Save the total price as JSON
-    total = Object.assign({ withShipping });
-
-    logCart(products, cartSum, withShipping);
+    option.querySelector(".cta").innerHTML = "Remove From Cart";
   } else {
     sessionStorage.removeItem(name);
+    option.querySelector(".cta").innerHTML = "Add To Cart";
   }
 };
 
 // ----------------------------------------------------------
-// EVENT HANDLERS
+// OPTION EVENT HANDLER
 
 // Toggles style change, extracts price, adds product to cart
 const handleOptionClick = event => {
@@ -116,12 +84,15 @@ const handleOptionClick = event => {
 // PAYMENT FORM
 
 let shippingOBJ = {};
+const paymentForm = document.getElementById("payment-form");
 
 const toggleFormClass = event => {
   let inputField = event.target || event.srcElement;
-  inputField.classList.add("filled");
+
+  // If user is currently typing in field, toggle active class
+  inputField.classList.add("active");
   let label = inputField.previousElementSibling;
-  label.classList.add("filled");
+  label.classList.add("active");
   label.style.display = label.style.display === "none" ? "none" : "";
 };
 
@@ -131,10 +102,57 @@ const handleFormInput = event => {
   let inputValue = event.target.value || event.srcElement;
   let inputName = event.target.name;
   shippingOBJ[inputName] = inputValue;
-  const shippingJSON = JSON.stringify(shippingOBJ);
-  console.log(shippingJSON);
+
+  if (!paymentForm.email.value.includes("@")) {
+    // paymentForm.email.classList.remove("valid");
+    paymentForm.email.classList.add("invalid");
+    alert("Please enter a valid email.");
+    return false;
+  }
 };
 
 const handleSubmit = event => {
-  // sumbits form data to JSON
+  event.preventDefault();
+
+  // Form Validation
+  if (paymentForm.email.value == "") {
+    paymentForm.email.classList.remove("valid");
+    paymentForm.email.classList.add("invalid");
+    alert("Please provide your email!");
+    return false;
+  }
+
+  // Loops through sessionStorage to get selected product data
+  const cartNames = Object.keys(sessionStorage);
+  const cartPrices = Object.values(sessionStorage);
+  const cartNums = cartPrices.map(el => parseFloat(el));
+  const cartSum = cartNums.reduce((sum, amount) => sum + amount);
+  const products = Object.assign({});
+  const shipping = 10.25;
+
+  // For every product, associate it with its corresponding price
+  cartNames.forEach(name => {
+    cartPrices.forEach(price => {
+      products[name] = price;
+    });
+  });
+
+  // Add Shipping
+  let withShipping = cartSum + shipping;
+
+  // Save the total price as JSON
+  total = Object.assign({ withShipping });
+
+  // logCart(products, cartSum, withShipping);
+  // Logs the cart JSON
+  orderOBJ.cart["products"] = products;
+  orderOBJ.cart.total["sub_total"] = `$${cartSum}`;
+  orderOBJ.cart.total["grand_total"] = `$${withShipping}`;
+  const cart = Object.assign({}, orderOBJ);
+  const orderJSON = JSON.stringify(cart);
+  console.log(orderJSON);
+
+  // Logs the shipping JSON
+  const finalShippingJSON = JSON.stringify(shippingOBJ);
+  console.log(finalShippingJSON);
 };
