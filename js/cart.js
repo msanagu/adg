9e4800b54d2eec08b8c26179c3fbf87a00c90376
Mary San Agustin <marycsanagustin@gmail.com>
2019-02-23 00:26:38 -0800

    Updates

--- js/cart.js
+++ js/cart.js
@@ -12,6 +12,9 @@ let orderOBJ = {
   }
 };
 
+let markupArr = [];
+let finalMarkup = "";
+
 // Adds green background and border when selected
 const toggleOption = option => {
   // Set initial state of option
@@ -23,10 +26,10 @@ const toggleOption = option => {
 
   // Set option state
   isSelected = !isSelected;
-  extractPrice(option.parentNode);
+  extractProductData(option.parentNode);
 };
 
-const extractPrice = option => {
+const extractProductData = option => {
   // Find the h4 element that contains the price
   const priceString = option.querySelector(".price");
   const price = priceString.innerHTML
@@ -43,12 +46,49 @@ const extractPrice = option => {
 
   // If option is selected, add its price to the sessionStorage
   if (option.classList.contains("selected")) {
+    const productImg = option.querySelector(".product-image");
+    const imagePath = productImg.getAttribute("src");
+    const markup = `<div class="cart-item">
+        <div class="item-img-border">
+          <img
+            class="item-img"
+            src="${imagePath}"
+            alt="Shopping Cart Item"
+          />
+        </div>
+        <div class="item-name">
+          <p>
+            ${name}
+          </p>
+        </div>
+        <p class="item-price">${price}</p>
+      </div>
+      <hr />`;
+
+    markupArr.push(markup);
     sessionStorage.setItem(name, priceNum);
     option.querySelector(".cta").innerHTML = "Remove From Cart";
   } else {
+    markupArr.pop();
     sessionStorage.removeItem(name);
     option.querySelector(".cta").innerHTML = "Add To Cart";
   }
+  const cartPrices = Object.values(sessionStorage);
+  const cartNums = cartPrices.map(el => parseFloat(el));
+  const cartSum = cartNums.reduce((sum, amount) => sum + amount);
+
+  console.log(cartPrices);
+  console.log(cartNums);
+  console.log(cartSum);
+
+  // Populate cart-item div with final markup
+  const cartWrapper = document.getElementById("cart-items");
+  const totalAmount = document.getElementById("total-amount");
+
+  finalMarkup = markupArr.join("");
+  cartWrapper.innerHTML = finalMarkup;
+  console.log(totalAmount);
+  totalAmount.innerHTML = `$${cartSum}`;
 };
 
 // ----------------------------------------------------------
